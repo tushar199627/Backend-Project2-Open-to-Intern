a8ca39c410145c632a1c7e497f70bca108ccf001
sumankumar323 <sangamsuman323@gmail.com>
2022-06-28 16:59:47 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://sangamsuman323:XVZrnDNPfS8c21p8@cluster0.bolaw.mongodb.net/projet2", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route)
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
---
+++ src/routes/route.js
@@ -0,0 +1,11 @@
+const express=require('express');
+const router=express.Router();
+const collegeController = require("../controller/collegeController");
+const internController = require("../controller/collegeController");
+
+
+
+
+
+
+module.exports = router;

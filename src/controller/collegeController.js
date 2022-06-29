47dcbc42b1644c6677b9f1772a8572d9300b4bb5
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-06-29 21:14:05 +0530

    Updated

--- src/controller/collegeController.js
+++ src/controller/collegeController.js
@@ -1,6 +1,6 @@
 const collegeModel = require("../models/collegeModel");
 const internModel = require("../models/internModel.js");
-//var validate = require('url-validator');
+
 
 let validUrl = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
 

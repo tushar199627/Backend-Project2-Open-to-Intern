e4da0f4fa85b6355276fbc446ae39ab1c23c75ba
Ranjeetsingh23 <Ranjeetsinghdua@gmail.com>
2022-06-30 11:31:32 +0530

    Updated

--- src/models/collegeModel.js
+++ src/models/collegeModel.js
@@ -12,8 +12,7 @@ const collegeSchema = new mongoose.Schema({
     fullName: {
         type: String,
         required:true,
-        trim:true,
-        sentencecase:true
+        trim:true
     },
 
     logoLink: {
